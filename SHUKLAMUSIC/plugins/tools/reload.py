94ecd19c39cbc8c9b2da570120aeae24ace3c4c5
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2026-07-19 22:12:39 +0530

    Update reload.py

--- SHUKLAMUSIC/plugins/tools/reload.py
+++ SHUKLAMUSIC/plugins/tools/reload.py
@@ -116,7 +116,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(8857291657)
+    & filters.user(8885151499)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
