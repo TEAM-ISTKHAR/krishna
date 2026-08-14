77b85257dd2053bb810297ec80b002260cca85ab
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2026-08-14 17:38:40 +0530

    Update mongo.py

--- SHUKLAMUSIC/core/mongo.py
+++ SHUKLAMUSIC/core/mongo.py
@@ -1,25 +1,36 @@
-# -----------------------------------------------
-# 🔸 StrangerMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2022 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# -----------------------------------------------
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_DB_URI
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+from pyrogram import Client
+
+import config
 from ..logging import LOGGER
 
-LOGGER(__name__).info("Connecting to your Mongo Database...")
-try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    LOGGER(__name__).info("Connected to your Mongo Database.")
-except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
-    exit()
+# Backup ke liye temp DB. Agar config me main DB nahi hai, tab ye kaam aayega.
+TEMP_MONGODB = ""
+
+if config.MONGO_DB_URI is None:
+    LOGGER(__name__).warning("No MONGO DB URL found. Please add it in your config vars!")
+    temp_client = Client(
+        "MusicBot",
+        bot_token=config.BOT_TOKEN,
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+    )
+    temp_client.start()
+    info = temp_client.get_me()
+    username = info.username
+    temp_client.stop()
+    
+    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+    _mongo_sync_ = MongoClient(TEMP_MONGODB)
+    mongodb = _mongo_async_[username]
+    pymongodb = _mongo_sync_[username]
+else:
+    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+    
+    # Aapke database ka naam yahan 'KavyaMusic' save hoga (Pehle Anon tha)
+    mongodb = _mongo_async_.KavyaMusic
+    pymongodb = _mongo_sync_.KavyaMusic
+    
+    LOGGER(__name__).info("✅ Connected to your Mongo Database Successfully.")
